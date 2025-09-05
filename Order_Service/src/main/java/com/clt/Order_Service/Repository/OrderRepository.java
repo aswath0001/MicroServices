28a752cce330f6fa950c1d586c847a1766bc9cf2
aswath0001 <rsaswaths38@gmail.com>
2025-09-05 15:05:38 +0530

    implemented API Gateway

--- Order_Service/src/main/java/com/clt/Order_Service/Repository/OrderRepository.java
+++ Order_Service/src/main/java/com/clt/Order_Service/Repository/OrderRepository.java
@@ -1,7 +1,6 @@
 package com.clt.Order_Service.Repository;
 
 import com.clt.Order_Service.Entity.Order;
-import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
